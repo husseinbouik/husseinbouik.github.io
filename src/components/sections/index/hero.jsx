b7542654cfd15fc4271057119798e4f70a1b0778
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-07-06 00:07:41 +0100

    chore: Update hero.jsx to fix section padding issue

--- src/components/sections/index/hero.jsx
+++ src/components/sections/index/hero.jsx
@@ -48,10 +48,10 @@ export default function Hero() {
 					wrapper={content.intro.wrapper}
 					repeat={Infinity}
 				/>
-				<section>
-					<h1 className={hero.header}>{content.header.name}</h1>
-					<h1 className={`${hero.header} ${hero.primaryDim}`}>{content.header.usp}</h1>
-				</section>
+		 <section style={{ paddingTop: '20px' }}>
+        <h1 className={hero.header}>{content.header.name}</h1>
+        <h1 className={`${hero.header} ${hero.primaryDim}`}>{content.header.usp}</h1>
+    </section>
 				<section>
 					<p className={`${hero.primaryBright} subtitle ${space(['verticalLrg'])}`}>
 						{content.paragraph}
